9ba9db5fb0ce4839f840c788316cc21272f2f755
Edward Forgacs <eddie.forgacs@gmail.com>
2020-04-17 22:50:35 -0400

    Add basic class format for weapons and enemies.

---
+++ enemies.py
@@ -0,0 +1,18 @@
+class Enemy:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    def is_alive(self):
+        return self.hp > 0
+
+
+class Lion(Enemy):
+    def __init__(self):
+        super().__init__(name="Lion", hp=10, damage=2)
+
+
+class Rhinoceros(Enemy):
+    def __init__(self):
+        super().__init__(name="Rhinoceros", hp=30, damage=15)
